276859676353905117a707c1f7119c4a6379fd0b
snify <snify@abv.bg>
2021-09-28 21:48:25 +0300

    Initial

---
+++ src/components/test-component/TestComponent.tsx
@@ -0,0 +1,7 @@
+import styles from './TestComponent.module.scss';
+
+const TestComponent = (): JSX.Element => {
+  return <h1 className={styles.blue}>Test component</h1>;
+};
+
+export default TestComponent;
